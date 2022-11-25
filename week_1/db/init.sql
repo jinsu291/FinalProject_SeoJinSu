e08e4f81351bda394455462ec95bbdc1e3e7f1e2
jinsu291 <wlstn291@naver.com>
2022-11-25 16:43:33 +0900

    make init sql

---
+++ week_1/db/init.sql
@@ -0,0 +1,9 @@
+# DEV DB
+DROP DATABASE IF EXISTS ssubooks__dev;
+CREATE DATABASE ssubooks__dev;
+USE ssubooks__dev;
+
+# TEST DB
+DROP DATABASE IF EXISTS ssubooks__test;
+CREATE DATABASE ssubooks__test;
+USE ssubooks__test

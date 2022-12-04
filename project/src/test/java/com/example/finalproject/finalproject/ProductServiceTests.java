b55231db1d9fc8109e872a3551a94ba00f0daec3
jinsu291 <wlstn291@naver.com>
2022-12-05 00:18:40 +0900

    make ProductServiceTests

---
+++ project/src/test/java/com/example/finalproject/finalproject/ProductServiceTests.java
@@ -0,0 +1,37 @@
+package com.example.finalproject.finalproject;
+
+import com.example.finalproject.finalproject.app.member.entity.Member;
+import com.example.finalproject.finalproject.app.member.repository.MemberRepository;
+import com.example.finalproject.finalproject.product.entity.Product;
+import com.example.finalproject.finalproject.product.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+public class ProductServiceTests {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("상품 등록")
+    void t1() {
+        Member author = memberRepository.findByUsername("user1").get();
+
+        Product product3 = productService.create(author, "상품명3", 50_000);
+        Product product4 = productService.create(author, "상품명4", 60_000);
+
+        assertThat(product3).isNotNull();
+        assertThat(product4).isNotNull();
+    }
+}

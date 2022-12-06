182e6681fdff1a76ee51cb128f24513ddcecb210
jinsu291 <wlstn291@naver.com>
2022-12-06 14:57:29 +0900

    implement RebateOrderItem list

--- project/src/main/java/com/example/finalproject/finalproject/app/rebate/controller/AdmRebateController.java
+++ project/src/main/java/com/example/finalproject/finalproject/app/rebate/controller/AdmRebateController.java
@@ -1,14 +1,18 @@
 package com.example.finalproject.finalproject.app.rebate.controller;
 
+import com.example.finalproject.finalproject.app.rebate.entity.RebateOrderItem;
 import com.example.finalproject.finalproject.app.rebate.service.RebateService;
 import com.example.finalproject.finalproject.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -33,10 +37,15 @@ public class AdmRebateController {
 
     @GetMapping("/rebateOrderItemList")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public String showRebateOrderItemList(String yearMonth) {
+    public String showRebateOrderItemList(String yearMonth, Model model) {
         if (yearMonth == null) {
             yearMonth = "2022-10";
         }
+
+        List<RebateOrderItem> items = rebateService.findRebateOrderItemsByPayDateIn(yearMonth);
+
+        model.addAttribute("items", items);
+
 
         return "adm/rebate/rebateOrderItemList";
     }

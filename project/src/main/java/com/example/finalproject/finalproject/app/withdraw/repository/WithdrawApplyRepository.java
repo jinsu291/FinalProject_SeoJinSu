d54ca5543f8c86893483af124b47bbd571f1f3d9
jinsu291 <wlstn291@naver.com>
2022-12-20 16:55:44 +0900

    make WithdrawApply, repository

---
+++ project/src/main/java/com/example/finalproject/finalproject/app/withdraw/repository/WithdrawApplyRepository.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.finalproject.app.withdraw.repository;
+
+import com.example.finalproject.finalproject.app.withdraw.entity.WithdrawApply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WithdrawApplyRepository extends JpaRepository<WithdrawApply, Long> {
+}

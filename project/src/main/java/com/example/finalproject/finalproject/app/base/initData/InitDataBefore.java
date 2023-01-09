1ec40c95913c1a80c99b7d606e87d50aa6f3c041
jinsu291 <wlstn291@naver.com>
2023-01-09 16:44:53 +0900

    add data in initDataBefore

--- project/src/main/java/com/example/finalproject/finalproject/app/base/initData/InitDataBefore.java
+++ project/src/main/java/com/example/finalproject/finalproject/app/base/initData/InitDataBefore.java
@@ -11,6 +11,7 @@ import com.example.finalproject.finalproject.app.post.service.PostService;
 import com.example.finalproject.finalproject.app.product.entity.Product;
 import com.example.finalproject.finalproject.app.product.service.ProductService;
 
+import java.util.Arrays;
 import java.util.List;
 
 public interface InitDataBefore {
@@ -41,16 +42,30 @@ public interface InitDataBefore {
         memberRepository.save(member1);
         Member member2 = memberService.join("user2", "1234", "user2@test.com", "user2");
 
-//        MyBook myBook1 = myBookService.create(member1, "책 1", 30_000);
-//        MyBook myBook2 = myBookService.create(member1, "책 2", 40_000);
-//        MyBook myBook3 = myBookService.create(member2, "책 3", 50_000);
-//        MyBook myBook4 = myBookService.create(member2, "책 4", 60_000);
-//        MyBook myBook5 = myBookService.create(member1, "책 5", 70_000);
-//        MyBook myBook6 = myBookService.create(member1, "책 6", 80_000);
-//        MyBook myBook7 = myBookService.create(member2, "책 7", 90_000);
-//        MyBook myBook8 = myBookService.create(member2, "책 8", 100_000);
+        postService.write(
+                member1,
+                "자바의 정석",
+                "# 목차",
+                "<h1>연산자 등</h1>",
+                "#자바 #정석"
+        );
 
-        Product product1 = productService.create(member1, "상품명1", 30_000, "자바", "IT");
+        postService.write(
+                member1,
+                "모던 딥다이브",
+                "# 목차",
+                "<h1>연산자 등</h1>",
+                "#모던딥다이브 #정석"
+        );
+
+        postService.write(member2, "제목 3", "내용 3", "내용 3", "#IT# 프론트엔드 #HTML #CSS");
+        postService.write(member2, "제목 4", "내용 4", "내용 4", "#IT #스프링부트 #자바");
+
+        Product product1 = productService.create(member2, "상품명2", 40_000, "스프링부트", "#IT #REACT");
+        Product product2 = productService.create(member1, "상품명3", 50_000, "REACT", "#IT #REACT");
+        Product product3 = productService.create(member1, "상품명3", 50_000, "REACT", "#IT #REACT");
+        Product product4 = productService.create(member2, "상품명4", 60_000, "HTML", "#IT #HTML");
+
 
         memberService.addCash(member1, 10_000, "충전__무통장입금");
         memberService.addCash(member1, 20_000, "충전__무통장입금");
@@ -58,37 +73,37 @@ public interface InitDataBefore {
         memberService.addCash(member1, 1_000_000, "충전__무통장입금");
         memberService.addCash(member2, 2_000_000, "충전__무통장입금");
 
-//        // 1번 주문 : 결제완료
-//        Order order1 = helper.order(member1, Arrays.asList(
-//                        product1,
-//                        product2
-//                )
-//        );
-//
-//        int order1PayPrice = order1.calculatePayPrice();
-//        orderService.payByRestCashOnly(order1);
-//
-//        // 2번 주문 : 결제 후 환불
-//        Order order2 = helper.order(member2, Arrays.asList(
-//                        product3,
-//                        product4
-//                )
-//        );
-//
-//        orderService.payByRestCashOnly(order2);
-//
-//        orderService.refund(order2);
-//
-//        // 3번 주문 : 결제 전
-//        Order order3 = helper.order(member2, Arrays.asList(
-//                        product1,
-//                        product2
-//                )
-//        );
-//
-//        cartService.addItem(member1, product1);
-//        cartService.addItem(member1, product2);
-//        cartService.addItem(member1, product3);
-//        cartService.addItem(member2, product4);
+        // 1번 주문 : 결제완료
+        Order order1 = helper.order(member1, Arrays.asList(
+                        product1,
+                        product2
+                )
+        );
+
+        int order1PayPrice = order1.calculatePayPrice();
+        orderService.payByRestCashOnly(order1);
+
+        // 2번 주문 : 결제 후 환불
+        Order order2 = helper.order(member2, Arrays.asList(
+                        product3,
+                        product4
+                )
+        );
+
+        orderService.payByRestCashOnly(order2);
+
+        orderService.refund(order2);
+
+        // 3번 주문 : 결제 전
+        Order order3 = helper.order(member2, Arrays.asList(
+                        product1,
+                        product2
+                )
+        );
+
+        cartService.addItem(member1, product1);
+        cartService.addItem(member1, product2);
+        cartService.addItem(member1, product3);
+        cartService.addItem(member2, product4);
     }
 }
